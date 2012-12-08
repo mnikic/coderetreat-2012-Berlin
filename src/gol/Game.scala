83cc95fb4327a9a30ac2e27f32b907e636d3a38e
mnikic <milos.nikic@groupon.com>
2012-12-08 22:21:52 +0100

    Made it working

--- src/gol/Game.scala
+++ src/gol/Game.scala
@@ -1,11 +1,39 @@
 package gol
 
-class Game (val state:List[(Int,Int)]){
-	def step() = {
-	  this
-	}
-	
-	def isActive(pos:(Int,Int)) = {
-	  false
-	}
+import Game._
+
+class Game(val state: List[Pos]) {
+
+  private def toActiveNeighbours(list: List[Pos]) = list.map(a => (a, lookupNeighbours(a).filter(state.contains(_))))
+
+  private def pickByRule(list: List[(Pos, List[Pos])])(rule: Rule) = list.filter(ap => rule(ap._2)).map(_._1)
+  
+  private val neighboursOfActive = state.flatMap(a => lookupNeighbours(a)).distinct
+
+  def step() = {
+    val activeByFirstThreeRules = pickByRule(toActiveNeighbours(state))(stayAlive _)
+    val activeByFourthRule = pickByRule(toActiveNeighbours(neighboursOfActive.filterNot(activeByFirstThreeRules.contains(_))))(becomeAlive _)
+
+    Game(activeByFirstThreeRules ::: activeByFourthRule)
+  }
+
+  def isActive(pos: Pos) = {
+    state.contains(pos)
+  }
+}
+
+object Game {
+  type Pos = (Int, Int)
+  type Rule = List[Pos] => Boolean
+  
+  val neighBouringPositions = List((-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1))
+  val allowedActiveNeighbourSize = List(2, 3)
+
+  def apply(state: List[Pos]) = new Game(state)
+
+  def lookupNeighbours(cell: Pos) = neighBouringPositions.map(pos => (pos._1 + cell._1, pos._2 + cell._2))
+
+  def stayAlive(activeNeighbours: List[Pos]) = allowedActiveNeighbourSize.contains(activeNeighbours.size)
+
+  def becomeAlive(activeNeighbours: List[(Int, Int)]) = activeNeighbours.size == 3
 }
--- src/gol/Something.scala
+++ src/gol/Something.scala
@@ -3,22 +3,60 @@ import org.scalatest.FunSuite
 import scala.collection.mutable.Stack
 import org.junit.runner.RunWith
 import org.scalatest.junit.JUnitRunner
- 
+
 @RunWith(classOf[JUnitRunner])
 class ExampleSuite extends FunSuite {
+
+  test("lookup neighbours on 0,0") {
+    assert(Game.lookupNeighbours((0, 0)) === Game.neighBouringPositions)
+  }
+
+  test("active should be active") {
+    val pos = (0, 0)
+    val active = List(pos)
+    val game = Game(active)
+
+    assert(game.isActive(pos) === true)
+  }
+
+  test("dead should stay dead if less then 3 active neighbours") {
+    val pos = (0, 0)
+    val active = List(pos)
+    val game = Game(active)
  
-  test("dies") {
-     val active = List((0,0))
-     val game = new Game(active)
-     
-     assert(game.step().isActive((0,0)) === false)
+    assert(game.isActive((1, 0)) === false)
+    assert(game.step.isActive((1, 0)) === false)
+  }
+
+  test("alone active should die after a step") {
+    val active = List((0, 0))
+    val game = Game(active)
+
+    assert(game.step.isActive((0, 0)) === false)
+  }
+
+  test("horizontal 3 rotate 90 degrees after a step") {
+    val active = List((0, 0), (-1, 0), (1, 0))
+    val game = Game(active)
+    val nextStep = game.step
+    
+    assert(nextStep.isActive((0, 0)) === true)
+    assert(nextStep.isActive((0, -1)) === true)
+    assert(nextStep.isActive((0, 1)) === true)
+    assert(nextStep.isActive((-1, 0)) === false)
+    assert(nextStep.isActive((1, 0)) === false)
   }
   
-  test("stays alive") {
-     val active = List((0,0),(-1,0),(1,0))
-     val game = new Game(active)
-   
-     assert(game.step().isActive((0,0)) === true)
+  test("vertical 3 rotate 90 degrees after a step") {
+    val active = List((0, 0), (0, 1), (0, -1))
+    val game = Game(active)
+    val nextStep = game.step
+    
+    assert(nextStep.isActive((0, 0)) === true)
+    assert(nextStep.isActive((0, -1)) === false)
+    assert(nextStep.isActive((0, 1)) === false)
+    assert(nextStep.isActive((-1, 0)) === true)
+    assert(nextStep.isActive((1, 0)) === true)
   }
 
 }
